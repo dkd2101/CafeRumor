86ad4ddaa0a808a75399dc645673524bab832e0d
Dylan Dinio <DDinio2101@gmail.com>
2025-02-27 04:03:16 -0500

    file organization. changed RecipeCard asset name, edited RecipeObject menu flow

--- Assets/Scripts/Recipe/RecipeSO.cs
+++ Assets/Scripts/Recipe/RecipeSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "RecipeData", menuName = "Recipes")]
+[CreateAssetMenu(fileName = "RecipeData", menuName = "Recipes/RecipeObject")]
 public class RecipeSO : ScriptableObject
 {
     public string dishName;
